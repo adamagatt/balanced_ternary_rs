8b6f329a1eefbd56aca048ef83a4b80bd4816ca4
Adam Gatt <adam.gatt@live.com.au>
2025-06-15 17:10:04 +0930

    Subtraction

--- src/number.rs
+++ src/number.rs
@@ -1,4 +1,4 @@
-use std::ops::Neg;
+use std::ops::{Add, Neg, Sub};
 use std::{cmp::min, fmt};
 
 use crate::trit::Trit;
@@ -7,27 +7,38 @@ use crate::trit::Trit;
 struct Number<const N: usize> ([Trit; N]);
 
 impl<const N: usize> Number<N> {
-
     fn new(encoded: &str) -> Self {
         let length = min(N, encoded.len());
 
-        // View character slice as slice of trits
-        let mut trits = encoded[..length].chars()
+        // View character slice as slice of trits, starting from right
+        // hand size (lowest significant trit)
+        let trits = encoded[..length].chars()
+            .rev()
             .map(Trit::from);
 
-        // Populate lowest N trits with decoded characters
+        Number::<N>::from_iter(trits)
+    }
+
+    fn from_iter(source: impl Iterator<Item = Trit>) -> Self {
+        let mut source_with_idx= source.enumerate();
+
+        // Populate lowest N trits with those provided from source
         let mut output = Number::<N>([(); N].map(|_| Trit::default()));
-        for i in (N-length)..N {
-            output.0[i] = trits.next().unwrap();
+        while let Some((idx, trit)) = source_with_idx.next() {
+            // Early exit if more trits are provided than the size of the Number
+            if idx == N {
+                break;
+            }
+
+            output.0[N-1-idx] = trit;
         }
-        output
+        output       
     }
 }
 
 impl<const N: usize> From<Number<N>> for i32 {
     fn from(number: Number<N>) -> i32 {
-        number.0.iter()
-            .rev()
+        number.0.iter().rev()
             .enumerate()
             .map(|(idx, trit)| match trit {
                 Trit::NEG => -3_i32.pow(idx as u32),
@@ -56,203 +67,29 @@ impl<const N: usize> Neg for Number<N> {
     }
 }
 
-// template <size_t N>
-// auto BT::Number<N>::operator+(const Number<N>& rhs) const -> Number<N> {
-//     Number<N> out;
+impl <const N: usize> Add for Number<N> {
+    type Output = Self;
 
-//     // Ideally a scan operation but none currently exist that allow for
-//     // two input collections to be zipped together
-//     std::ranges::transform(
-//         value | std::views::reverse,
-//         rhs.value | std::views::reverse,
-//         out.value.rbegin(),
-//         // Return sum of trits and hold carry trit in accumulator to
-//         // propagate up to the next index. Mutable lambda to allow held
-//         // carry trit to be updated.
-//         [sumResult=SumResult{}](const Trit& lhs, const Trit& rhs) mutable {
-//             sumResult = addTrits(lhs, rhs, sumResult.carry);
-//             return sumResult.result;
-//         }
-//     );
+    fn add(self, rhs: Self) -> Self::Output {
+        let reverse_result_trits = self.0.iter().rev()
+            .zip(rhs.0.iter().rev())
+            .scan(Trit::ZERO, |carry, (lhs, rhs)| {
+                let sum_result = lhs.add_with_carry(rhs, carry);
+                *carry = sum_result.carry;
+                Some(sum_result.result)
+            });
+        
+        Number::<N>::from_iter(reverse_result_trits)
+    }
+}
 
-//     return out;
-// }
+impl <const N: usize> Sub for Number<N> {
+    type Output = Self;
 
-// template <size_t N>
-// auto BT::Number<N>::operator+=(const Number<N>& rhs) {
-
-//     // Ideally a scan operation but none currently exist that allow for
-//     // two input collections to be zipped together
-//     std::ranges::transform(
-//         value | std::views::reverse,
-//         rhs.value | std::views::reverse,
-//         value.rbegin(),
-//         // Return sum of trits and hold carry trit in accumulator to
-//         // propagate up to the next index. Mutable lambda to allow held
-//         // carry trit to be updated.
-//         [sumResult=SumResult{}](const Trit& lhs, const Trit& rhs) mutable {
-//             sumResult = addTrits(lhs, rhs, sumResult.carry);
-//             return sumResult.result;
-//         }
-//     );
-// }
-
-// template <size_t N>
-// auto BT::Number<N>::operator-(const Number<N>& rhs) const -> Number<N> {
-//     return *this + (-rhs);
-// }
-
-// template <size_t N>
-// auto BT::Number<N>::operator-=(const Number<N>& rhs) {
-//     *this += (-rhs);
-// }
-
-// template <size_t N>
-// auto BT::Number<N>::operator*(const Number<N>& rhs) const -> Number<N> {
-//     Number<N> out;
-
-//     // Balanced ternary multiplication is a simple shift-and-adding operation
-//     // made easy as a -1 trit in the lhs current index only requires the rhs
-//     // to be negated (flip all trits) before adding. Without a useful enumerate()
-//     // function or elegant way to zip our array with std::iota() we fall back on
-//     // multiple-initialisation and update in a simple for loop to create and update
-//     // an iterator together with a left-shifting copy of the rhs value.
-//     for (auto it = value.rbegin(), rhs_shifted = rhs; it != value.rend(); ++it, rhs_shifted <<= 1) {
-//         if (*it == Trit::POS) {
-//             out += rhs_shifted;
-//         } else if (*it == Trit::NEG) {
-//             out -= rhs_shifted;
-//         }
-//     }
-
-//     return out;
-// }
-
-// template <size_t N>
-// auto BT::Number<N>::operator*=(const Number<N>& rhs) {
-//     // Based on the shift-and-add approach to multiplication I don't see an
-//     // obvious way to do a more efficient in-place multiplication operator.
-//     // We need a third spare variable to build the complete result and then
-//     // copy it into our own array, or otherwise we make a copy of our own 
-//     // array before zeroing it out and perform the shift-and-add in-place
-//     // on that array.
-//     *this = (*this * rhs);
-// }
-
-// template <size_t N>
-// auto BT::Number<N>::operator/(const Number<N>& divisor) const -> Number<N> {
-//     if (divisor == ZERO) {
-//         std::cerr << "Attempt to divide by zero" << std::endl;
-//         std::exit(EXIT_FAILURE);
-//     }
-
-//     // Integer division implemented with a repeated subtraction approach. We
-//     // convert numerator and divisor to positive to perform the division, and
-//     // then decide whether to flip the result based on if they originally had
-//     // different signs.
-
-//     bool numerator_is_negative = (*this) < ZERO;
-//     auto abs_remainder = numerator_is_negative
-//         ? -(*this)
-//         : (*this);
-
-//     bool divisor_is_negative = divisor < ZERO;
-//     const auto abs_divisor = divisor_is_negative
-//         ? -divisor
-//         : divisor;
-
-//     BT::Number<N> quotient = ZERO;
-//     while (abs_remainder >= abs_divisor) {
-//         abs_remainder -= abs_divisor;
-//         ++quotient;
-//     }
-
-//     return (numerator_is_negative ^ divisor_is_negative)
-//         ? -quotient
-//         : quotient;
-// }
-
-// template <size_t N>
-// auto BT::Number<N>::operator/=(const Number<N>& divisor) {
-//     *this = (*this / divisor);
-// }
-
-// template <size_t N>
-// auto BT::Number<N>::operator<<(size_t positions) const -> Number<N> {
-//     // Early exit if we left-shift far enough that our number just becomes zero
-//     if (positions >= N) {
-//         return Number<N>();
-//     }
-
-//     Number<N> out;
-
-//     // Left shift is just copying the correct trits from our value to the
-//     // start of our zero-initialised output number 
-//     std::copy(
-//         std::next(value.begin(), positions), value.end(),
-//         out.value.begin()
-//     );
-
-//     return out;
-// }
-
-// template <size_t N>
-// auto BT::Number<N>::operator<<=(size_t positions) {
-//     // Early exit if we left-shift far enough that our number just becomes zero
-//     if (positions >= N) {
-//         std::ranges::fill(value, Trit::ZERO);
-//         return;
-//     }
-
-//     // An in-place left-shift is achieved by rotating our value array by the
-//     // specified number of positions and then zeroing out the least-significant
-//     // trits.
-//     std::rotate(value.begin(), std::next(value.begin(), positions), value.end());
-//     // Zeroing performed right-to-left for slightly easier math
-//     std::fill(value.rbegin(), std::next(value.rbegin(), positions), Trit::ZERO);
-// }
-
-// template <size_t N>
-// BT::Number<N>::operator int32_t() const {
-//     int32_t result = 0;
-
-//     // Without a useful enumerate() function (or way to elegantly zip collection
-//     // elements with std::iota()) we use multiple initialisation and update to
-//     // track an iterator to the current trit (right-to-left) as well as the
-//     // absolute value of that position (tripling for each index). We add or
-//     // subtract that value (or do nothing) based on the trit encountered. With
-//     // an enumerate() iterator adaptor we could instead calculate the value with
-//     // pow(3, idx), perhaps more wastefully depending on compiler optimisation.
-//     // We could also technically determine our index in-loop with std::distance
-//     // but somehow this seems more wasteful even though for an std::array with
-//     // a random-access iterator the calculation of distance is O(1).
-//     for (auto val = 1, it = value.rbegin(); it != value.rend(); val*=3, ++it) {
-//         if (*it == Trit::POS) {
-//             result += val;
-//         } else if (*it == Trit::NEG) {
-//             result -= val;
-//         }
-//     }
-
-//     return result;
-// }
-
-// template <size_t M>
-// auto operator<<(std::ostream& os, const BT::Number<M>& rhs) -> std::ostream& {
-//     for (BT::Trit trit : rhs.value) {
-//         if (trit == BT::Trit::POS) {
-//             os << '+';
-//         } else if (trit == BT::Trit::NEG) {
-//             os << '-';
-//         } else {
-//             os << '0';
-//         }
-//     }
-
-//     os << " (" << static_cast<int32_t>(rhs) << ")";
-
-//     return os;
-// }
+    fn sub(self, rhs: Self) -> Self::Output {
+        self + (-rhs)
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -307,4 +144,15 @@ mod tests {
         // Only one representation of zero, and so negative zero is still zero
         assert_eq!(-num_0, num_0);
     }
+
+    #[test]
+    fn binary_operations() {
+        let num_23 = Number::<8>::new("+0--");
+        let num_33 = Number::<8>::new("++-0");
+
+    assert_eq!(num_23 + num_33, Number::<8>::new("+-0+-")); // Sum to 56
+    assert_eq!(num_23 - num_33, Number::<8>::new("-0-")); // Difference is -10
+    // EXPECT_EQ(num_33 - num_23, BT::Number<8>{"+0+"}); // Difference is 10
+    // EXPECT_EQ(num_23 * num_33, BT::Number<8>{"+00+0+0"}); // Product is 759
+}
 }
